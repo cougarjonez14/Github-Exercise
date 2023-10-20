babd6263c99f9a26915806130e8f0a46976cf4cd
StationAstral <cougarjonez@gmail.com>
2023-10-20 15:39:10 -0700

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by 1 on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
